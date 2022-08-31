9c9bbc3f2a978607a47c67a5bb3b515ab0f662be
Raimonda <raime.jasaityte@gmail.com>
2022-08-31 21:58:35 +0300

    dalis 5 užduoties

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,9 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+// function filterDogOwers = users.filter()
+
+
+// const adults = users.filter(person => person.age >= 18);
+// console.log(adults)
